872f567cfed162dc9bcdf5eee11076b45f77d8f9
Sobhan1996 <mniknami@uwaterloo.ca>
2020-04-10 23:23:16 -0400

    changed the parameters of create_tfrecords.py

--- datasets/openwebtext/create_tfrecords.py
+++ datasets/openwebtext/create_tfrecords.py
@@ -10,14 +10,14 @@ from tqdm import tqdm
 
 import encoder
 
-base_dir = "/home/connor/2/newspaper" # Path to where your .txt files are located
+base_dir = "datasets/CNN_DM_dataset/dm_stories_tokenized" # Path to where your .txt files are located
 files_per = 175000 # 175000 ~ 200-300MB
-name = "openwebtext-newspaper" # Name of output files will be name_i.tfrecords where i is the number of the file
-output_dir = "/home/connor/out"
+name = "cnn_dm" # Name of output files will be name_i.tfrecords where i is the number of the file
+output_dir = "tfrecords"
 log_dir = "logs"
 files = glob.glob(os.path.join(base_dir, "**/*.txt"))
 processes = 64 # Number of encoding processes to run
-encoder_path = "gs://openwebtext/stuff/encoder" # Path to encoder files
+encoder_path = "encoder" # Path to encoder files
 minimum_size = 25
 
 def _int64_feature(value):
